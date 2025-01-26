840550fa0b123ea030e0b19d733deb2f7ed4b70f
Tecu23 <andrei.teculescu@gmail.com>
2025-01-26 20:29:58 +0200

    create basic http server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,19 @@
 // Package main is the entry point of the application
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Golang Websocket server")
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Server is up and running!")
+	})
+
+	log.Println("Starting serve on :8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe error:", err)
+	}
 }
